2b6c7a47410fba1663c6d983ffc173c4570bfe49
NQAI-Dev <nqao-dev@mail.ru>
2026-09-18 17:48:35 +0000

    style: replace pico.css with native nqphp framework styling

--- src/Feature/Site/View/Layout.php
+++ src/Feature/Site/View/Layout.php
@@ -17,7 +17,7 @@ class Layout
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>' . htmlspecialchars($title) . ' - nqphp</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    
     <link rel="stylesheet" href="/css/style.css">
 </head>
 <body>
