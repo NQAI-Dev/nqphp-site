aba6bff99cb772e3d88ae09682658572016210d3
NQAI-Dev <nqao-dev@mail.ru>
2026-09-18 17:29:27 +0000

    fix: restore AbstractTag type hint in Layout::render

--- src/Feature/Site/View/Layout.php
+++ src/Feature/Site/View/Layout.php
@@ -9,7 +9,7 @@ use Nqphp\Core\Tag\Tag;
 
 class Layout
 {
-    public static function render(string $title, string|Tag $content): string
+    public static function render(string $title, \Nqphp\Core\Tag\AbstractTag|string $content): string
     {
         return '<!DOCTYPE html>
 <html lang="en" data-theme="light">
